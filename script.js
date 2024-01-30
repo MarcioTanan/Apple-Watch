d4a26d7a9b20a63e2f672c5b2acdca16fddfbf36
Marcio Tanan <marcio-tanan@hotmail.com>
2024-01-30 20:52:36 -0300

    add event onclick

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const imagemVisualizacao = document.getElementById('imagem-visualizacao')
+
 const verdeCipreste = {
     nome: 'Verde-cipreste',
     pasta: 'imagens-verde-cipreste'
@@ -21,4 +23,14 @@ const estelar = {
 const rosaClaro = {
     nome: 'Rosa-claro',
     pasta: 'imagens-rosa-claro'
+}
+
+const opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro]
+
+let imagemSelecionada = 1
+
+function trocarImagem(){
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id
+    imagemSelecionada = idOpcaoSelecionada.charAt(0)
+    imagemVisualizacao.src = './imagens/opcoes-cores/imagens-azul-inverno/imagem-' + imagemSelecionada + '.jpeg'
 }
